0fec0b94a1b16f4a3d32148946799a4a444e4062
Courtney Tern <catern19@gmail.com>
2021-04-20 18:20:54 -0400

    reformatting segment

--- assets/util/md5generator.sh
+++ assets/util/md5generator.sh
@@ -12,3 +12,15 @@ for file in $( cat ./1.txtFilenames.txt ); do
     wget $line -O- | md5 -r >> ./MD5_$file
   done
 done
+
+#### !!! If not working, check that the txt file is properly encoded. may present issues on mac ####
+#### `od -c filename.txt` to see if there's '\r' at the end of every line.
+#### if so, use `tr -d '\r' filename.txt` and save the output to the file
+
+## Now reformat to get into nice commas separated format
+for file in $( cat ./1.txtFilenames.txt ); do
+  awk '{if( NR % 2 != 0 ) { printf $0"," }
+  else { print $0 } }' < ./MD5_$file > ./MD5_$file.csv
+done
+
+# Now copy the columns into the TOMARKDOWN csvs and convert to markdown again
